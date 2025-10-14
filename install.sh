bbb1c579707d3646718be2051f9e5bc815259bee
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-10-14 09:19:17 +0000

    finalizing automatic setup script

--- install.sh
+++ install.sh
@@ -6,14 +6,18 @@
 set -e
 
 # Set up ROS Jazzy Repositories
-sudo apt install software-properties-common
-sudo add-apt-repository universe
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
-# Install ros base, build tools and moveit
-sudo apt update && sudo apt install ros-dev-tools ros-jazzy-ros-base ros-jazzy-moveit 
+# Install ros-base and rosdep for dependencies management
+sudo apt-get update && sudo apt-get install -y python3-rosdep ros-jazzy-ros-base
+# Setup environment
+source /opt/ros/jazzy/setup.bash
+# Instantiate rosdep
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src -y --ignore-src
 # Rollback rviz-common version due to moveit-setup-assistant incompatiability
 # See https://github.com/moveit/moveit2/issues/3541
 wget --no-check-certificate -O /tmp/ros-rviz.deb https://snapshots.ros.org/jazzy/2025-05-23/ubuntu/pool/main/r/ros-jazzy-rviz-common/ros-jazzy-rviz-common_14.1.11-1noble.20250520.201719_amd64.deb
@@ -21,9 +25,11 @@ sudo dpkg -i /tmp/ros-rviz.deb
 rm /tmp/ros2-apt-source.deb
 rm /tmp/ros-rviz.deb
 
-# Optionally set up X-Forwarding for GUI tools
-sudo apt install openssh-server
+# Set up X-Forwarding for GUI tools
+sudo apt-get install -y openssh-server
 touch ~/.Xauthority
 
 # Export automatic ros sourcing
 echo "source /opt/ros/jazzy/setup.bash" >> ~/.bashrc
+echo "export ROS_LOG_DIR='/tmp/ros'" >> ~/.bashrc
+echo "source /workspaces/VGU_Project_ROS/install/setup.bash" >> ~/.bashrc
