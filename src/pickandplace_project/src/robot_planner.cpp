ce84246aef6992344d5ffb73101bfcf53e5c6e0e
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-10-27 04:43:48 +0000

    update code styles... expect many changes

--- src/pickandplace_project/src/robot_planner.cpp
+++ src/pickandplace_project/src/robot_planner.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+
 #include "ros/init.h"
 #include "ros/node_handle.h"
 #include "ros/service_server.h"
@@ -6,18 +7,20 @@
 
 using IntegrationService = ros_unity_integration::IntegrationService;
 
-bool test(IntegrationService::Request& request, IntegrationService::Response& response)
-{
+bool test(
+    IntegrationService::Request&  request,
+    IntegrationService::Response& response
+) {
     printf("Hello!");
     return true;
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     ros::init(argc, argv, "robot_planner");
-    ros::NodeHandle node_handle;
+    ros::NodeHandle    node_handle;
 
-    ros::ServiceServer service = node_handle.advertiseService("IntegrationService", test);
+    ros::ServiceServer service =
+        node_handle.advertiseService("IntegrationService", test);
     ROS_INFO("Service Ready");
     ros::spin();
 
