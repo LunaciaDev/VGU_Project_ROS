c38f045114ce41615857c54c0b3de2f98f2c6a58
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-11-04 06:40:55 +0000

    add a joint_state_controller to publish robot joint states to /joint_state

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -30,3 +30,4 @@ echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 echo "source /workspaces/VGU_Project_ROS/devel/setup.bash" >> ~/.bashrc
 echo "export ROS_LOG_DIR='/tmp/ros'" >> ~/.bashrc
 echo export ROSCONSOLE_FORMAT=\''[${severity}] [${walltime:%Y-%m-%d %H:%M:%S}] [${node}]: ${message}'\' >> ~/.bashrc
+echo "export DISABLE_ROS1_EOL_WARNINGS = 1" >> ~/.bashrc
--- src/fake_ur10e_control/src/ur10e_fake_interface.hpp
+++ src/fake_ur10e_control/src/ur10e_fake_interface.hpp
@@ -1,11 +1,21 @@
 #include "ros/duration.h"
-#include "ros_control_boilerplate/generic_hw_interface.h"
+#include "ros_control_boilerplate/sim_hw_interface.h"
 
-class UR10eHWInterface : public ros_control_boilerplate::GenericHWInterface {
-    public:
-    UR10eHWInterface(ros::NodeHandle& node_handle, urdf::Model* urdf_model = NULL);
+class UR10eHWInterface : public ros_control_boilerplate::SimHWInterface {
+   public:
+    UR10eHWInterface(
+        ros::NodeHandle& node_handle,
+        urdf::Model*     urdf_model = NULL
+    );
 
+    virtual void init();
     virtual void read(ros::Duration& elapsed_time);
     virtual void write(ros::Duration& elapsed_time);
     virtual void enforceLimits(ros::Duration& period);
+
+   private:
+    virtual void positionControlSimulation(
+        ros::Duration&    elapsed_time,
+        const std::size_t joint_id
+    );
 };
