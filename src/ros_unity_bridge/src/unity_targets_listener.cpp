dc984d1a28597b88f23c3239bd911c8280a47f1a
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-11-06 03:54:57 +0000

    add gripper action, the gripper controller cannot close yet due to self collision

--- src/ros_unity_bridge/src/unity_targets_listener.cpp
+++ src/ros_unity_bridge/src/unity_targets_listener.cpp
@@ -10,16 +10,21 @@ using MoveGroupInterface = moveit::planning_interface::MoveGroupInterface;
 using UnityRequest = ros_unity_messages::UnityRequest;
 
 void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
-    static const std::string PLANNING_GROUP = "robot_arm";
+    static const std::string ARM_PLANNING_GROUP = "robot_arm";
+    static const std::string GRIPPER_PLANNING_GROUP = "robot_gripper";
     static const ros::Duration sleep_timer = ros::Duration(1, 500000);
     ROS_INFO("Received target message from Unity.");
 
-    MoveGroupInterface move_group_interface(PLANNING_GROUP);
+    MoveGroupInterface arm_move_group_interface(ARM_PLANNING_GROUP);
+    MoveGroupInterface gripper_move_group_interface(GRIPPER_PLANNING_GROUP);
 
     // Allow replanning if scene change, would come in useful when scene change?
-    move_group_interface.allowReplanning(true);
+    arm_move_group_interface.allowReplanning(true);
     // Allow replan attempt in case the planner simply didnt find a path, there are time when it does that
-    move_group_interface.setNumPlanningAttempts(5);
+    arm_move_group_interface.setNumPlanningAttempts(5);
+
+    // Same config for gripper
+    gripper_move_group_interface.setNumPlanningAttempts(5);
 
     // Uncomment if you want to check Unity joint control script
     // debug_joint(move_group_interface);
@@ -27,22 +32,26 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
     // Let's start plan and execute!
     // Pre-Grasp pose
     ROS_INFO("Planning and executing pre-grasp pose");
-    move_group_interface.setPoseTarget(message->pick_pose, "arm_tcp_link");
-    if (move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
+    arm_move_group_interface.setPoseTarget(message->pick_pose, "arm_tcp_link");
+    if (arm_move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
         ROS_ERROR("Failed to move to pre_grasp pose, exiting");
         return;
     }
     sleep_timer.sleep();
     ROS_INFO("Pre-grasp pose executed");
 
+    // Open the gripper
+    gripper_move_group_interface.setNamedTarget("gripper_open");
+    gripper_move_group_interface.move();
+
     // Pick pose
     {
         auto pick_pose = geometry_msgs::Pose(message->pick_pose);
-        pick_pose.position.z = pick_pose.position.z - 0.15;
+        pick_pose.position.z = pick_pose.position.z - 0.18;
 
         ROS_INFO("Planning and executing pick pose");
-        move_group_interface.setPoseTarget(pick_pose, "arm_tcp_link");
-        if (move_group_interface.move() !=
+        arm_move_group_interface.setPoseTarget(pick_pose, "arm_tcp_link");
+        if (arm_move_group_interface.move() !=
             moveit::core::MoveItErrorCode::SUCCESS) {
             ROS_ERROR("Failed to move to pick pose, exiting");
             return;
@@ -51,10 +60,14 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
     sleep_timer.sleep();
     ROS_INFO("Pick pose executed");
 
+    // Close the gripper
+    gripper_move_group_interface.setNamedTarget("gripper_close");
+    gripper_move_group_interface.move();
+
     // Pickup Pose
     ROS_INFO("Planning and executing pickup pose");
-    move_group_interface.setPoseTarget(message->pick_pose, "arm_tcp_link");
-    if (move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
+    arm_move_group_interface.setPoseTarget(message->pick_pose, "arm_tcp_link");
+    if (arm_move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
         ROS_ERROR("Failed to move to pickup pose, exiting");
         return;
     }
@@ -63,8 +76,8 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
 
     // Pre-place Pose
     ROS_INFO("Planning and executing pre-place pose");
-    move_group_interface.setPoseTarget(message->place_pose, "arm_tcp_link");
-    if (move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
+    arm_move_group_interface.setPoseTarget(message->place_pose, "arm_tcp_link");
+    if (arm_move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
         ROS_ERROR("Failed to move to pre_grasp pose, exiting");
         return;
     }
@@ -76,8 +89,8 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
         auto place_pose = geometry_msgs::Pose(message->place_pose);
         place_pose.position.z = place_pose.position.z - 0.15;
         ROS_INFO("Planning and executing place pose");
-        move_group_interface.setPoseTarget(place_pose, "arm_tcp_link");
-        if (move_group_interface.move() !=
+        arm_move_group_interface.setPoseTarget(place_pose, "arm_tcp_link");
+        if (arm_move_group_interface.move() !=
             moveit::core::MoveItErrorCode::SUCCESS) {
             ROS_ERROR("Failed to move to place pose, exiting");
             return;
@@ -86,22 +99,30 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
     sleep_timer.sleep();
     ROS_INFO("Place pose executed");
 
+    // Open the gripper
+    gripper_move_group_interface.setNamedTarget("gripper_open");
+    gripper_move_group_interface.move();
+
     // Lift-up Pose
     ROS_INFO("Planning and executing lift-up pose");
-    move_group_interface.setPoseTarget(message->place_pose, "arm_tcp_link");
-    if (move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
+    arm_move_group_interface.setPoseTarget(message->place_pose, "arm_tcp_link");
+    if (arm_move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
         ROS_ERROR("Failed to move to lift-up pose, exiting");
         return;
     }
     sleep_timer.sleep();
     ROS_INFO("Lift-up pose executed");
 
+    // Return gripper to neutral
+    gripper_move_group_interface.setNamedTarget("gripper_neutral");
+    gripper_move_group_interface.move();
+
     // Return to starting position
     {
         std::vector<double> joint_group_position;
         joint_group_position.resize(6, 0);
-        move_group_interface.setJointValueTarget(joint_group_position);
-        if (move_group_interface.move() !=
+        arm_move_group_interface.setJointValueTarget(joint_group_position);
+        if (arm_move_group_interface.move() !=
             moveit::core::MoveItErrorCode::SUCCESS) {
             ROS_ERROR("Failed to move to all-zero pose, exiting");
             return;
