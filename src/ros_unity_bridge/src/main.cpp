29de7b5e0bf7570586030b14447baf84ded928de
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-11-06 06:49:34 +0000

    merging updating static objects to the planning code

--- src/ros_unity_bridge/src/main.cpp
+++ src/ros_unity_bridge/src/main.cpp
@@ -3,9 +3,7 @@
 #include "ros/node_handle.h"
 #include "ros/spinner.h"
 #include "ros/subscriber.h"
-#include "ros_unity_messages/SyncUnityObjects.h"
 #include "ros_unity_messages/UnityRequest.h"
-#include "static_objects_listener.hpp"
 #include "unity_targets_listener.hpp"
 
 int main(int argc, char** argv) {
@@ -13,10 +11,10 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "unity_bridge");
     ros::NodeHandle node_handle;
 
-    // 2 thread, one thread for subscriber callback, one thread for moveit
-    // 2 thread for unity objects, probably
+    // 3 thread, one thread for subscriber callback, one thread for moveit, one
+    // for static objects?
     // callback
-    ros::AsyncSpinner spinner = ros::AsyncSpinner(4);
+    ros::AsyncSpinner spinner = ros::AsyncSpinner(3);
 
     spinner.start();
 
@@ -24,14 +22,8 @@ int main(int argc, char** argv) {
     ROS_INFO("Registering unity_targets subscriber");
     const ros::Subscriber unity_targets_subscriber =
         node_handle.subscribe<ros_unity_messages::UnityRequest>(
-            "/unity_bridge/unity_targets", 1000, unity_targets_subs_handler
+            "/unity_bridge/unity_targets", 0, unity_targets_subs_handler
         );
-
-    // Unity Objects subscriber
-    ROS_INFO("Registering unity_static_objects subscriber");
-    const ros::Subscriber unity_objects_subscriber = node_handle.subscribe<ros_unity_messages::SyncUnityObjects>(
-        "/unity_bridge/unity_static_objects", 1000, unity_static_objects_sub_handler
-    );
 
     ROS_INFO("unity_bridge node ready for action.");
 
--- src/ros_unity_bridge/src/unity_targets_listener.cpp
+++ src/ros_unity_bridge/src/unity_targets_listener.cpp
@@ -2,25 +2,76 @@
 
 #include "joint_debug.hpp"
 #include "moveit/move_group_interface/move_group_interface.h"
+#include "moveit/planning_scene_interface/planning_scene_interface.h"
 #include "moveit/utils/moveit_error_code.h"
 #include "ros/console.h"
 #include "ros/duration.h"
+#include "ros_unity_messages/UnityObject.h"
 
 using MoveGroupInterface = moveit::planning_interface::MoveGroupInterface;
+using PlanningSceneInterface =
+    moveit::planning_interface::PlanningSceneInterface;
 using UnityRequest = ros_unity_messages::UnityRequest;
+using UnityObject = ros_unity_messages::UnityObject;
+
+static void update_planning_scene(
+    const std::vector<UnityObject> unity_objects,
+    PlanningSceneInterface         planning_scene_interface
+) {
+    const auto known_ids = planning_scene_interface.getObjects();
+    static const std::string PLANNING_FRAME = "arm_base_link";
+    std::vector<moveit_msgs::CollisionObject> scene_objects_list;
+
+    // for each object received from Unity
+    for (const UnityObject object : unity_objects) {
+        // create a corresponding MoveIt Message
+        moveit_msgs::CollisionObject scene_object;
+        scene_object.id = object.id.data;
+        scene_object.header.frame_id = PLANNING_FRAME;
+        scene_object.operation = scene_object.ADD;
+
+        shape_msgs::SolidPrimitive primitive;
+        primitive.type = primitive.BOX;
+        primitive.dimensions.resize(3);
+        // During the conversion to ROS coordinate space, the y value is
+        // inverted z in Unity
+        primitive.dimensions[primitive.BOX_X] = object.scale.x;
+        // -x in Unity, notice the negative
+        primitive.dimensions[primitive.BOX_Y] = -object.scale.y;
+        // y in Unity
+        primitive.dimensions[primitive.BOX_Z] = object.scale.z;
+        scene_object.primitives.push_back(primitive);
+
+        // define the pose
+        // PLANNING_FRAME is positioned at (0,0,0) for both side so no
+        // transformation needed
+        geometry_msgs::Pose primitive_pose;
+        primitive_pose.orientation = object.orientation;
+        primitive_pose.position = object.position;
+        scene_object.primitive_poses.push_back(primitive_pose);
+
+        // Add the message to the list of messages to be sent
+        scene_objects_list.push_back(scene_object);
+    }
+
+    // Apply the object to planning_scene (Blocking until finished!)
+    planning_scene_interface.applyCollisionObjects(scene_objects_list);
+}
 
 void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
-    static const std::string ARM_PLANNING_GROUP = "robot_arm";
-    static const std::string GRIPPER_PLANNING_GROUP = "robot_gripper";
+    static const std::string   ARM_PLANNING_GROUP = "robot_arm";
+    static const std::string   GRIPPER_PLANNING_GROUP = "robot_gripper";
     static const ros::Duration sleep_timer = ros::Duration(1, 500000);
     ROS_INFO("Received target message from Unity.");
 
-    MoveGroupInterface arm_move_group_interface(ARM_PLANNING_GROUP);
-    MoveGroupInterface gripper_move_group_interface(GRIPPER_PLANNING_GROUP);
+    MoveGroupInterface     arm_move_group_interface(ARM_PLANNING_GROUP);
+    MoveGroupInterface     gripper_move_group_interface(GRIPPER_PLANNING_GROUP);
+    const PlanningSceneInterface planning_scene_interface;
 
     // Allow replanning if scene change, would come in useful when scene change?
     arm_move_group_interface.allowReplanning(true);
-    // Allow replan attempt in case the planner simply didnt find a path, there are time when it does that
+    // Allow replan attempt in case the planner simply didnt find a path, there
+    // are time when it does that
     arm_move_group_interface.setNumPlanningAttempts(5);
 
     // Same config for gripper
@@ -29,11 +80,15 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
     // Uncomment if you want to check Unity joint control script
     // debug_joint(move_group_interface);
 
+    // Build the planning scene
+    update_planning_scene(message->static_objects, planning_scene_interface);
+
     // Let's start plan and execute!
     // Pre-Grasp pose
     ROS_INFO("Planning and executing pre-grasp pose");
     arm_move_group_interface.setPoseTarget(message->pick_pose, "arm_tcp_link");
-    if (arm_move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
+    if (arm_move_group_interface.move() !=
+        moveit::core::MoveItErrorCode::SUCCESS) {
         ROS_ERROR("Failed to move to pre_grasp pose, exiting");
         return;
     }
@@ -60,6 +115,8 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
     sleep_timer.sleep();
     ROS_INFO("Pick pose executed");
 
+    // [TODO]: Attach the cube here to arm_tcp_link
+
     // Close the gripper
     gripper_move_group_interface.setNamedTarget("gripper_close");
     gripper_move_group_interface.move();
@@ -67,7 +124,8 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
     // Pickup Pose
     ROS_INFO("Planning and executing pickup pose");
     arm_move_group_interface.setPoseTarget(message->pick_pose, "arm_tcp_link");
-    if (arm_move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
+    if (arm_move_group_interface.move() !=
+        moveit::core::MoveItErrorCode::SUCCESS) {
         ROS_ERROR("Failed to move to pickup pose, exiting");
         return;
     }
@@ -77,7 +135,8 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
     // Pre-place Pose
     ROS_INFO("Planning and executing pre-place pose");
     arm_move_group_interface.setPoseTarget(message->place_pose, "arm_tcp_link");
-    if (arm_move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
+    if (arm_move_group_interface.move() !=
+        moveit::core::MoveItErrorCode::SUCCESS) {
         ROS_ERROR("Failed to move to pre_grasp pose, exiting");
         return;
     }
@@ -103,10 +162,13 @@ void unity_targets_subs_handler(const UnityRequest::ConstPtr& message) {
     gripper_move_group_interface.setNamedTarget("gripper_open");
     gripper_move_group_interface.move();
 
+    // [TODO]: Detach the cube here from arm_tcp_link
+
     // Lift-up Pose
     ROS_INFO("Planning and executing lift-up pose");
     arm_move_group_interface.setPoseTarget(message->place_pose, "arm_tcp_link");
-    if (arm_move_group_interface.move() != moveit::core::MoveItErrorCode::SUCCESS) {
+    if (arm_move_group_interface.move() !=
+        moveit::core::MoveItErrorCode::SUCCESS) {
         ROS_ERROR("Failed to move to lift-up pose, exiting");
         return;
     }
